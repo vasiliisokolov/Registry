577a5ec64e22d2d0ccaf1c2dfe00fb520ee4fdc4
VasiliiSokolov <vasilysokolov@mail.ru>
2023-01-28 21:25:37 +0300

    Добавьте файлы проекта.

---
+++ Registry.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+int main()
+{
+    std::string input;
+    bool working = true;
+    int patientNumber = 0;
+    std::map<int, std::string> list;
+    std::cout << "Registry!" << std::endl;
+    while (working)
+    {
+        std::cin >> input;
+        if (input == "Exit")
+        {
+            working = false;
+        }
+        else if (input == "Next")
+        {
+
+        }
+        else
+        {
+            patientNumber++;
+            list.insert(std::pair<int, std::string> (patientNumber, input));
+
+        }
+    }
+    
+}
+
